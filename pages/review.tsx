a105e0ce8e90095fc120dc253fe0a0bc4c98ad06
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-11 18:38:09 +0400

    отзывы

--- pages/review.tsx
+++ pages/review.tsx
@@ -1,38 +1,57 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
-const review = (props) => {
-	const data = props.data;
-	return (
-		<>
-			<Head>
-				<title>Бургеры | Отзывы</title>
-			</Head>
-			<div>
-				<h1>Отзывы</h1>
-				<div className='reviews'>
-					{!!data.length &&
-						data.slice(0, 20).map((comment) => {
-							return (
-								<div key={comment.id} className='review'>
-									{comment.id}
-									{`${comment.body.slice(0, 90)}...`}
-								</div>
-							);
-						})}
-				</div>
-			</div>
-		</>
-	);
+
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
 };
 
-export async function getServerSideProps() {
-	const res = await fetch('https://jsonplaceholder.typicode.com/comments');
-	const arr = await res.json();
-	return {
-		props: {
-			data: arr,
-		},
-	};
-}
 
-export default review;
+type ReviewProps = {
+  data: Comment[];
+};
+
+
+const Review: React.FC<ReviewProps> = ({ data }) => {
+  return (
+    <>
+      <Head>
+        <title>Бургеры | Отзывы</title>
+      </Head>
+      <div>
+        <h1>Отзывы</h1>
+        <div className='reviews'>
+          {!!data.length &&
+            data.slice(0, 20).map((comment) => {
+              return (
+                <div key={comment.id} className='review'>
+                  {comment.id}
+                  {`${comment.body.slice(0, 90)}...`}
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<ReviewProps> = async () => {
+
+	
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const arr: Comment[] = await res.json();
+
+
+  return {
+    props: {
+      data: arr,
+    },
+  };
+};
+
+export default Review;
