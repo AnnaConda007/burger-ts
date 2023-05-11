63dafe852635ed84789581dee9d65dda17ec7738
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-11 19:23:08 +0400

    коментарии

--- pages/review.tsx
+++ pages/review.tsx
@@ -8,15 +8,15 @@ type Comment = {
   name: string;
   email: string;
   body: string;
-};
-
+};// создается алиас объекта 
 
 type ReviewProps = {
   data: Comment[];
+  //массив из объектов типа Comment
 };
 
 
-const Review: React.FC<ReviewProps> = ({ data }) => {
+const Review: React.FC<ReviewProps> = ({ data }) => { // Review ПРИНИМАЕТ компонент ReviewProps, то есть массив объектов
   return (
     <>
       <Head>
@@ -41,12 +41,11 @@ const Review: React.FC<ReviewProps> = ({ data }) => {
 };
 
 export const getServerSideProps: GetServerSideProps<ReviewProps> = async () => {
-
-	
+  //????? тут в GetServerSideProps  часть<ReviewProps>, указыввает на то, что функция возвращает
+  // но в const Review: React.FC<ReviewProps>, запись такая же, но указывает на то, что компонент ПРИНИМАЕТ
+  // то есть запсь общая - _название:типЭлемента<возвращает/принимает>.Возвращает или принимает, зависит от самого элемента? То есть компонент всегда принимает, функция возвращает.
   const res = await fetch('https://jsonplaceholder.typicode.com/comments');
-  const arr: Comment[] = await res.json();
-
-
+  const arr: Comment[] = await res.json();  
   return {
     props: {
       data: arr,
