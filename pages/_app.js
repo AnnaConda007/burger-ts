b7980c9727cba7da261501b350eb21a05eb2ce89
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-11 11:06:52 +0400

    относительные адреса для стилей и layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import '@/styles/globals.css';
-import Layout from '@/components/layout';
-import Head from 'next/head';
+import '../styles/globals.css';
+import Layout from '../components/layout';
 export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
